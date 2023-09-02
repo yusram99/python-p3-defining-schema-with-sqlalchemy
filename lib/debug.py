84d527ac742e357942186eba5a555c268523db37
Your Name <youremail@example.com>
2023-09-02 15:33:12 +0300

    commit message

--- lib/debug.py
+++ lib/debug.py
@@ -7,4 +7,4 @@ from sqlalchemy_sandbox import Student
 engine = create_engine('sqlite:///students.db')
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    import pdb; pdb.set_trace()
